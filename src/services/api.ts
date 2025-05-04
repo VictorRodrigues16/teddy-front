e990318c6c26a58d43284a2b1bf721f21ae35c07
Victor rodrigues <vitorr.29@hotmail.com>
2025-05-04 01:04:39 -0300

    Fix: backend url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://teddy-backend-kyel.onrender.com",
+  baseURL: "https://teddy-back.fly.dev",
   headers: {
     "Content-Type": "application/json",
   },
