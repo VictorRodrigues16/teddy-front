340523b294adcca2684630af391dd067a8bab28c
Victor rodrigues <vitorr.29@hotmail.com>
2025-05-04 00:28:50 -0300

    Feat: url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://teddy-backend-kyel.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
